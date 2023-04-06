ebc6028f5ab51bb95bba2972d985342ac28abdf2
RICHNVRDIE <109940057+RICHNVRDIE@users.noreply.github.com>
2023-04-06 13:16:01 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     resource_group_name  = "rg2"
     storage_account_name = "winstrom"
     container_name       = "container"
-    key                  = "prod.terraform.tfstate"
+    key                  = "terraform.tfstate"
     use_msi              = true
     subscription_id      = "b5f306d2-971d-4c83-83c2-2c7b69f22671"
     tenant_id            = "9fead588-3912-437d-9b8d-03b1e59028d3"
