f6f86e6fe6f44a10611ba33804dc620b4daecff3
RICHNVRDIE <109940057+RICHNVRDIE@users.noreply.github.com>
2023-04-06 13:16:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -85,16 +85,3 @@ resource "azurerm_linux_virtual_machine" "linux" {
     version   = "latest"
   }
 }
-resource "azurerm_virtual_machine_extension" "extension" {
-  name                 = "myvm-extension"
-  virtual_machine_id   = azurerm_linux_virtual_machine.linux.id
-  publisher            = "Microsoft.Azure.Extensions"
-  type                 = "CustomScript"
-  type_handler_version = "2.0"
-
-  settings = <<SETTINGS
-    {
-        "script": "${filebase64("script.sh")}"
-    }
-SETTINGS
-}
