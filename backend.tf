032f3b751453ba50d248efd7e4585c0f6f7a9967
RICHNVRDIE <109940057+RICHNVRDIE@users.noreply.github.com>
2023-04-06 13:34:29 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,7 +4,6 @@ terraform {
     storage_account_name = "winstrom"
     container_name       = "container"
     key                  = "terraform.tfstate"
-    use_oidc              = true
     subscription_id      = "b5f306d2-971d-4c83-83c2-2c7b69f22671"
     tenant_id            = "9fead588-3912-437d-9b8d-03b1e59028d3"
     client_id            = "debcdec1-a295-4545-8615-4bf16756cd80"
